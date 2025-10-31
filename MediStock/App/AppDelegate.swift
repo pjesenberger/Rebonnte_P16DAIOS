c5ac3d620b6f6710808f2155bc27a77413ccbe0c
pascaljesenberger <pascal@jesenberger.fr>
2025-10-31 15:53:42 +0100

    Make Firebase not configure while testing

--- MediStock/App/AppDelegate.swift
+++ MediStock/App/AppDelegate.swift
@@ -12,7 +12,9 @@ import Firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        #if !TESTING
         FirebaseApp.configure()
+        #endif
         return true
     }
 }
